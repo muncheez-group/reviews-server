ecc0c6eecd0ea0d5e58316e17b6b47465c8f912b
OliverHan <ohan88@gmail.comgit config --global user.email ohan88@gmail.com>
2018-04-16 15:20:10 -0700

    Fixing git issues.

--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Button, Icon, Card, Row, Col, Modal } from 'react-materialize';
+import LinesEllipsis from 'react-lines-ellipsis'
 
 export default class Review extends React.Component {
  constructor(props) {
    super(props);
+   this.state = {
+     expanded: false
+   };
+
+   this.expandDescription = this.expandDescription.bind(this);
+ }
+ expandDescription() {
+   this.setState({
+     expanded: !this.state.expanded
+   })
  }
 
  generateStars(num) {
@@ -21,24 +32,39 @@ export default class Review extends React.Component {
    }
    return result
  }
-render() {
-  var review = this.props.review[0]
-  var stars = this.generateStars(review.rating)
 
-  return (
-    <div className="review-container">
-      <div className="review-profile-pic">
-      <img src={review.profile_photo_url} width={70} height={70}/>
-      </div>
-      <div className="review-column">
-        <div className="review-column-name">{review.author_name}</div>
-        <div className="review-column-date">{review.relative_time_description}</div>
-        <div className="review-column-text">{stars}  {review.text}
+ render() {
+    var review = this.props.review[0]
+    var stars = this.generateStars(review.rating)
+    var description = `${stars}  ${review.text}`;
+
+    let component = null
+    if (!this.state.exapnded) {
+      component = <LinesEllipsis
+       text= {description}
+       maxLine='3'
+       ellipsis= '... See More'
+       trimRight
+       basedOn='letters'
+       />
+    } if (this.state.expanded) {
+      component = description
+    }
+
+    return (
+      <div className="review-container">
+        <div className="review-profile-pic">
+        <img src={review.profile_photo_url} width={70} height={70}/>
         </div>
-      </div>
-  </div>
-  )}
-}
+        <div className="review-column" onClick={() => this.expandDescription()}>
+          <div className="review-column-name">{review.author_name}</div>
+          <div className="review-column-date">{review.relative_time_description}</div>
+          <div className="review-column-text">{component}</div>
+        </div>
+    </div>
+    )
+  }
+ }
 
 
 window.Review = Review
