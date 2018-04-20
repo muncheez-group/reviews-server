75496d00394bc9d99c9cc0f60e0dede384d4dc22
OliverHan <ohan88@gmail.comgit config --global user.email ohan88@gmail.com>
2018-04-20 01:53:41 -0700

    Added hard coded states.

--- client/components/Description.jsx
+++ client/components/Description.jsx
@@ -6,10 +6,11 @@ export default class Description extends React.Component {
     super(props);
     this.state = {
       title: 'RICH TABLE',
+      type: 'Rustic Northern Californian restaurant',
       foodScore: '4.6',
       decorScore: '4.0',
       serviceScore: '4.4',
-      descriptionBody: 'lorem epsum stuff and stuff'
+      descriptionBody: `'Inventive” yet “approachable”, this Hayes Valley “destination” by Evan and Sarah Rich “wows” with its “unique”, “delectable” Californian dishes matched by a “killer wine list” and “warm” service in a minimalist (and “loud”) space adorned with salvaged barn wood; it's “hard to get into” (though walk-ins can sit at the bar or the communal table) and the “pricey”, “locally sourced” menu “changes” often, but “if the stars align, you will leave sockless`,
     };
 
   }
@@ -20,8 +21,8 @@ export default class Description extends React.Component {
   render() {
     return (
       <div className="reviews-container">
-        <div className="description-title">RICH TABLE</div>
-        <div className="description-type">Rustic Northern Californian restaurant</div>
+        <div className="description-title">{this.state.title}</div>
+        <div className="description-type">{this.state.type}</div>
         <div className="description-details-container">
           <div className="description-details">New American</div><div className='desription-details-seperator'>·</div>
           <div className="description-details">Hayes Valley</div><div className='desription-details-seperator'>·</div>
@@ -31,18 +32,17 @@ export default class Description extends React.Component {
         <div className="description-divider"></div>
         <div className="description-apateez-header">THE APATEEA REVIEW</div>
         <div className="description-ratings-container">
-          <div className="description-ratings-food">4.6
+          <div className="description-ratings-food">{this.state.foodScore}
             <div className="description-ratings-label">FOOD</div>
           </div>
-          <div className="description-ratings-decor">4.0
+          <div className="description-ratings-decor">{this.state.decorScore}
             <div className="description-ratings-label">DECOR</div>
           </div>
-          <div className="description-ratings-service">4.4
+          <div className="description-ratings-service">{this.state.serviceScore}
             <div className="description-ratings-label">SERVICE</div>
           </div>
         </div>
-        <div className="description-body">
-          “Inventive” yet “approachable”, this Hayes Valley “destination” by Evan and Sarah Rich “wows” with its “unique”, “delectable” Californian dishes matched by a “killer wine list” and “warm” service in a minimalist (and “loud”) space adorned with salvaged barn wood; it's “hard to get into” (though walk-ins can sit at the bar or the communal table) and the “pricey”, “locally sourced” menu “changes” often, but “if the stars align, you will leave sockless.”
+        <div className="description-body">{this.state.descriptionBody}
         </div>
       </div>
     );
