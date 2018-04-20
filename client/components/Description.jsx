15d0dfd246b8feee009e5fe13d3650a031459320
OliverHan <ohan88@gmail.comgit config --global user.email ohan88@gmail.com>
2018-04-20 01:48:59 -0700

    Hard coded description component.

--- client/components/Description.jsx
+++ client/components/Description.jsx
@@ -5,7 +5,11 @@ export default class Description extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      title: 'RICH TABLE',
+      foodScore: '4.6',
+      decorScore: '4.0',
+      serviceScore: '4.4',
+      descriptionBody: 'lorem epsum stuff and stuff'
     };
 
   }
@@ -18,15 +22,28 @@ export default class Description extends React.Component {
       <div className="reviews-container">
         <div className="description-title">RICH TABLE</div>
         <div className="description-type">Rustic Northern Californian restaurant</div>
-        <div className="description-details">New American · Hayes Valley · $$$</div>
+        <div className="description-details-container">
+          <div className="description-details">New American</div><div className='desription-details-seperator'>·</div>
+          <div className="description-details">Hayes Valley</div><div className='desription-details-seperator'>·</div>
+          <div className="description-details">$$$</div>
+        </div>
+
         <div className="description-divider"></div>
         <div className="description-apateez-header">THE APATEEA REVIEW</div>
         <div className="description-ratings-container">
-          <div className="description-ratings-food">4.6 FOOD</div>
-          <div className="description-ratings-decor">4.0 DECOR</div>
-          <div className="description-ratings-service">4.4 SERVICE</div>
+          <div className="description-ratings-food">4.6
+            <div className="description-ratings-label">FOOD</div>
+          </div>
+          <div className="description-ratings-decor">4.0
+            <div className="description-ratings-label">DECOR</div>
+          </div>
+          <div className="description-ratings-service">4.4
+            <div className="description-ratings-label">SERVICE</div>
+          </div>
         </div>
-        <div className="description-body">LOTS OF STUFF HERE</div>
+        <div className="description-body">
+          “Inventive” yet “approachable”, this Hayes Valley “destination” by Evan and Sarah Rich “wows” with its “unique”, “delectable” Californian dishes matched by a “killer wine list” and “warm” service in a minimalist (and “loud”) space adorned with salvaged barn wood; it's “hard to get into” (though walk-ins can sit at the bar or the communal table) and the “pricey”, “locally sourced” menu “changes” often, but “if the stars align, you will leave sockless.”
+        </div>
       </div>
     );
   }
--- client/index.js
+++ client/index.js
@@ -1,5 +1,12 @@
-// import Reviews from './components/Reviews.jsx';
-// // import Description from './components/Description.jsx';
-//
-// window.Reviews = Reviews;
-// // window.Description = Description;
+import Reviews from './components/Reviews.jsx';
+import Description from './components/Description.jsx';
+
+ReactDOM.render(
+  React.createElement(Reviews),
+  document.getElementById('reviews')
+);
+
+ReactDOM.render(
+  React.createElement(Description),
+  document.getElementById('description')
+);
