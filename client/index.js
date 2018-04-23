0a58bc8fca666b63d4bd10210d452b3c639175bf
OliverHan <ohan88@gmail.comgit config --global user.email ohan88@gmail.com>
2018-04-22 22:10:36 -0700

    Worked on description.

--- client/index.js
+++ client/index.js
@@ -1,12 +1,2 @@
 import Reviews from './components/Reviews.jsx';
 import Description from './components/Description.jsx';
-
-ReactDOM.render(
-  React.createElement(Reviews),
-  document.getElementById('reviews')
-);
-
-ReactDOM.render(
-  React.createElement(Description),
-  document.getElementById('description')
-);
