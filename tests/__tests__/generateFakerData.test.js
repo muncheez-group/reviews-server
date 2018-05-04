e007d43688181171d792df7e9a17891ec6db2dfa
rthai <ahnamiko@gmail.com>
2018-05-04 12:24:22 -0700

    add data generator test

---
+++ tests/__tests__/generateFakerData.test.js
@@ -0,0 +1,27 @@
+const generateFakerData = require('../../db/__mocks__/generateFakerData');
+
+const data = JSON.parse(generateFakerData());
+console.log(data)
+describe('Faker Data Generator', () => {
+
+  test('should generate fake data with appropriate value types', () => {
+    expect(typeof data).toBe('object');
+    expect(typeof data.name).toBe('string');
+    expect(typeof data.price_level).toBe('number'); 
+    expect(typeof data.neighborhood).toBe('string');
+    expect(typeof data.city).toBe('string');
+    expect(typeof data.street).toBe('string');
+    expect(typeof data.rating).toBe('number'); 
+    expect(Array.isArray(data.reviews)).toBe(true); 
+  });
+
+  test('price_level should be an integer before 1 and 4, inclusive', () => {
+    expect(data.price_level).toBeGreaterThanOrEqual(1); 
+    expect(data.price_level).toBeLessThanOrEqual(4); 
+  });
+
+  test('rating should be an integer before 1 and 4, inclusive', () => {
+    expect(data.rating).toBeGreaterThanOrEqual(1); 
+    expect(data.rating).toBeLessThanOrEqual(5); 
+  });
+})
