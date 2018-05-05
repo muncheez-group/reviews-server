437993e223444d33b6afaf9e23b260fdf2a9681f
rthai <ahnamiko@gmail.com>
2018-05-05 16:32:45 -0700

    creates restaurant descriptions csv

---
+++ db/writeCSVDescriptonFile.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const generateDescription = require('./generateCSVData.js');
+
+const stream = fs.createWriteStream('./data/descriptions.csv');
+const header = 'place_id,name,price_level,neighborhood,city,street,rating\n';
+
+stream.write(header);
+
+const writeTenMillionTimes = () => {
+  let i = 5;
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      if (i === 0) {
+        stream.write(generateDescription(), 'utf-8', () => {
+          stream.end();
+        });
+      } else {
+        ok = stream.write(generateDescription(), 'utf-8');
+        stream.write('\n');
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      stream.once('drain', write);
+    }
+  }
+  write();
+};
+
+writeTenMillionTimes();
