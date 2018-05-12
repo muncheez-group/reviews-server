55dcb98113ea44a826f27bfe64e73b5bfa411f2f
rthai <ahnamiko@gmail.com>
2018-05-12 10:34:43 -0700

    setup postgres server

---
+++ db/pg_server.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  database: 'muncheez_restaurants',
+});
+
+const getReviews = (placeId) => {
+  const q = `select * from reviews where place_id = ${placeId}`;
+  return pool.query(q)
+}
+
+const getDescription = (placeId) => {
+  const q = `select * from restaurants where place_id = ${placeId}`;
+  return pool.query(q)
+};
+
+module.exports = {
+  getReviews,
+  getDescription
+}
