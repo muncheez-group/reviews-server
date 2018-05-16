7c36fe944ab3b33d1a53063e0db167a506cb6ec4
rthai <ahnamiko@gmail.com>
2018-05-16 12:31:13 -0700

    wrap components into a parent component, App

--- client/index.jsx
+++ client/index.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Reviews from './components/Reviews.jsx';
 import Description from './components/Description.jsx';
-import './style.css';
 
-ReactDOM.render(
-  React.createElement(Reviews),
-  document.getElementById('reviews')
-);
+const App = () => (
+  <div>
+    <Description />
+    <Reviews />
+  </div>
+)
 
-ReactDOM.render(
-  React.createElement(Description),
-  document.getElementById('description')
-);
+export default App;
